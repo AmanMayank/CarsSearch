b81f2dc82ed46cda52781ad3f1dbb7e67c84529b
AmanMayank <aman.msingh87@gmail.com>
2023-06-12 18:34:07 +0530

    UserCarEntryAndDeletion

--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -4,24 +4,24 @@ const carsSlice = createSlice({
   name: "cars",
   initialState: {
     searchTerm: "",
-    cars: [],
+    data: [],
   },
   reducers: {
     changeSearchTerm(state, action) {
       state.searchTerm = action.payload;
     },
     addCar(state, action) {
-      state.cars.push({
+      state.data.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid(),
       });
     },
     removeCar(state, action) {
-      const updatedList = state.cars.filter((car) => {
+      const updatedList = state.data.filter((car) => {
         return car.id !== action.payload;
       });
-      state.cars = updatedList;
+      state.data = updatedList;
     },
   },
 });
